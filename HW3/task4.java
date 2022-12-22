b70adce391b26f9e05b499b9a38a0cf689fe9a4a
Anastasiya <piterskayaz@mail.ru>
2022-12-22 15:10:11 +0300

    task4

---
+++ HW3/task4.java
@@ -0,0 +1,15 @@
+package HW3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class task4 {
+    //Извлечь элемент (по указанному индексу) из заданного списка
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("Кот", "Собака", "Корова", "Лошадь", "Петух"));
+          System.out.println(list);
+        System.out.println(list.get(4));
+
+    }
+}
